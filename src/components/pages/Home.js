e26f45a8b239a9835b4e388cd1597d6beee7ce07
Gazi Salahuddin <71849295+Salahuddin-Gazi@users.noreply.github.com>
2020-12-30 02:32:29 +0600

    Newly added
    30 Dec

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import "../../App.css";
+import HeroSection from "../HeroSection";
+// import { HeroSection } from "../HeroSection.js";
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
